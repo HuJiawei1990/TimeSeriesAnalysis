2adc504ed93126d1fcf6d7763d4dce4bb2d42a7e
HU Jiawei <hujiaw_cn@126.com>
2018-01-14 00:02:20 +0800

    keep editting

--- src/TimeSeries/TimeSeries.py
+++ src/TimeSeries/TimeSeries.py
@@ -14,6 +14,7 @@
 import sys
 import logging
 import pandas as pd
+import numpy as np
 
 logger = logging.getLogger('datageek')
 
@@ -24,12 +25,12 @@ class TimeSeries(object):
             logger.error('Index & Values series have different lengths.')
             logger.info("Index series length = [%i]" % len(index))
             logger.info("Value series length = [%i]" % len(value))
-            raise "Failed to created TimeSeries object."
+            raise Exception("Failed to created TimeSeries object.")
         else:
             self.index = list(index)
             self.value = value
-            self.data = pd.DataFrame(data=self.value, columns=['value'])
-            self.data.index = self.index
+            self._data = pd.DataFrame(data=self.value, columns=['value'])
+            self._data.index = self.index
             self.length = len(index)
             self.min_time = min(self.index)
             self.max_time = max(self.index)
@@ -50,37 +51,63 @@ class TimeSeries(object):
         
         if self.isSorted & self.ascending == ascending:
             logger.info("--> Time series has already sorted. Passed")
+
         else:
             aux = self
             
-            aux.data = self.data.sort_index(ascending=ascending, inplace=inplace)
-            aux.index = aux.data.index
-            aux.value = aux.data.values
-            
+            aux.data = self._data.sort_index(ascending=ascending, inplace=inplace)
+            aux.index = aux._data.index
+            aux.value = aux._data.values
+
         logger.info("Time Series sorting process ends successfully.")
         
         if not inplace:
             return aux
         
         self = aux
-        
-        
-    
+
     
     def getDataframe(self):
         """
         :return: pandas dataframe style of time series
         """
-        return self.data
+        return self._data
     
     
-    def getStep(self, mode='auto', maxSample=100):
-        if self.step > 0:
-            pass
-        
+    def getStep(self, mode='auto', max_samples=100, alpha = 0.2):
+        logger.info('Time Series is going to reset its step value.')
+
+
         if not self.isSorted:
-            self.sort(desc=False)
-            
-        maxSample = min(self.length, maxSample)
+            logger.info('--> Time Series is not sore')
+            self.sort(ascending=True)
+        max_samples = min(self.length, max_samples)
+        ## compute the steps between two timestamps
+        steps = [(self.index[i+1] - self.index[i]) for i in range(max_samples-1)]
+        n_steps = len(steps)
+        default_steps = list(range(1, 11)) + [15, 20, 30] + list(range(60, 3601, 60))
+
+        step_avg = sum(steps) / n_steps
+        step_std = sum([i*i for i in steps]) / n_steps - (sum(steps) / n_steps) ** 2
+
+        if mode == 'auto':
+
+            if step_std < alpha * step_avg:
+                abs_diff = [abs(step_avg - default_step) for default_step in default_steps]
+                self.step = default_steps[np.argmin(abs_diff)]
+                logger.info('--> Time Series step is reset to %i' % self.step)
+            else:
+                self.step = 0
+                logger.warning('--> Time Series step is not suggested to change. Reset to 0.')
+
+
+
+
+
+
+
+
+
+
         
         
